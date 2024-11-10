f9cf4d819c6a1670fac13161793fe5a860d617d9
eimarinin <marinin2003@yandex.ru>
2024-11-10 23:01:19 +0300

    lesson_type

--- balder_schedule_app/lib/screens/edit/lesson/lesson_create_screen.dart
+++ balder_schedule_app/lib/screens/edit/lesson/lesson_create_screen.dart
@@ -1,6 +1,8 @@
 import 'package:balder_schedule_app/generated/l10n.dart';
+import 'package:balder_schedule_app/screens/edit/lesson/lesson_segments.dart';
 import 'package:balder_schedule_app/utils/padded_screen.dart';
 import 'package:balder_schedule_app/widgets/edit/lesson/lesson_field.dart';
+import 'package:balder_schedule_app/widgets/edit/lesson/lesson_segment.dart';
 import 'package:balder_schedule_app/widgets/page_header_child.dart';
 
 import 'package:flutter/material.dart';
@@ -52,7 +54,7 @@ class _LessonCreateScreenState extends State<LessonCreateScreen> {
   }
 }
 
-class LessonCreateContent extends StatelessWidget {
+class LessonCreateContent extends StatefulWidget {
   final TextEditingController nameController;
   final TextEditingController classController;
 
@@ -61,6 +63,14 @@ class LessonCreateContent extends StatelessWidget {
     required this.nameController,
     required this.classController,
   });
+
+  @override
+  State<LessonCreateContent> createState() => _LessonCreateContentState();
+}
+
+class _LessonCreateContentState extends State<LessonCreateContent> {
+  String _selectedLessonType = 'Лекция';
+  final TextEditingController _customController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -73,11 +83,63 @@ class LessonCreateContent extends StatelessWidget {
             children: [
               LessonField(
                 labelText: 'Название предмета',
-                controller: nameController,
+                controller: widget.nameController,
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6),
+                  color: Theme.of(context).colorScheme.surfaceContainer,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Тип занятия',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Gap(12),
+                    LessonSegments(
+                      selected: {_selectedLessonType},
+                      onSelectionChanged: (newSelection) {
+                        if (_customController.text.isEmpty) {
+                          setState(() {
+                            _selectedLessonType = newSelection.isNotEmpty
+                                ? newSelection.first
+                                : '';
+                          });
+                        } else {
+                          setState(() {
+                            _selectedLessonType = _customController.text;
+                          });
+                        }
+                        print('Выбран тип занятия: $_selectedLessonType');
+                      },
+                      segments: [
+                        LessonSegment(value: 'Лекция', text: 'Лекция'),
+                        LessonSegment(value: 'Практика', text: 'Практика'),
+                        LessonSegment(
+                            value: 'Лаб. работа', text: 'Лаб. работа'),
+                      ],
+                      customController: _customController,
+                      validator: (value) {
+                        if (_customController.text.isNotEmpty) {
+                          return null;
+                        }
+
+                        if (value == null || value.isEmpty) {
+                          return 'Пожалуйста, выберите тип занятия';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
               ),
               LessonField(
                 labelText: 'Аудитория',
-                controller: classController,
+                controller: widget.classController,
                 isNumeric: true,
               ),
             ],
@@ -86,5 +148,11 @@ class LessonCreateContent extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _customController.dispose();
+    super.dispose();
   }
 }
---
+++ balder_schedule_app/lib/screens/edit/lesson/lesson_segments.dart
@@ -0,0 +1,78 @@
+import 'package:balder_schedule_app/widgets/edit/lesson/lesson_segment.dart';
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class LessonSegments extends FormField<String> {
+  final TextEditingController customController;
+
+  LessonSegments({
+    super.key,
+    required Set<String> selected,
+    required ValueChanged<Set<String>> onSelectionChanged,
+    required List<LessonSegment> segments,
+    required this.customController,
+    super.validator,
+  }) : super(
+          initialValue: selected.isNotEmpty ? selected.first : '',
+          builder: (FormFieldState<String> state) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: SegmentedButton<String>(
+                        emptySelectionAllowed: true,
+                        style: SegmentedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                          ),
+                        ),
+                        segments: segments,
+                        selected: selected,
+                        onSelectionChanged: (newSelection) {
+                          if (customController.text.isNotEmpty) {
+                            customController.clear();
+                          }
+
+                          state.didChange(newSelection.isNotEmpty
+                              ? newSelection.first
+                              : '');
+
+                          onSelectionChanged(newSelection);
+                        },
+                      ),
+                    ),
+                    const Gap(12),
+                    TextFormField(
+                      controller: customController,
+                      decoration: InputDecoration(
+                        labelText: 'Другой тип занятия',
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          state.didChange('');
+                        }
+                        onSelectionChanged({});
+                      },
+                    ),
+                    if (state.hasError)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          state.errorText ?? '',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.error,
+                          ),
+                        ),
+                      ),
+                  ],
+                );
+              },
+            );
+          },
+        );
+}
